3f40897af0cb4ffb1cc16f134db6ca8622c8e2c8
Pablo Westphalen <gotjin@gmail.com>
2012-04-23 05:44:17 -0300

    Aeae

--- src/main/java/com/jin/tpdb/Search.java
+++ src/main/java/com/jin/tpdb/Search.java
@@ -1,24 +1,14 @@
 package com.jin.tpdb;
-
+import entities.Pessoa;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-// <add1>
-//import javax.persistence.PersistenceContext;
-// </add1>
-
-// modified below line. added the full "com.jin.tpdb" package to what was an import of just "entities"
-import com.jin.tpdb.entities.Pessoa;
-
 
 public class Search extends HttpServlet {
 	
-	//@PersistenceContext(unitName="jin")
-	//private EntityManager em;
-
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		
 		response.setContentType("text/html");
@@ -29,7 +19,6 @@ public class Search extends HttpServlet {
 		
 		out.println("You searched for: " + q);
 		
-		//<add2>
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("jin");
 		EntityManager em = factory.createEntityManager();		
 		Pessoa p = new Pessoa();
@@ -38,12 +27,8 @@ public class Search extends HttpServlet {
 		em.getTransaction().begin();
 		em.persist(p);
 		em.getTransaction().commit();
-		//removed "System" from line below
-		out.println("<br />commited");
-		//</add2>
-		
-	}
-	
+		out.println("<br />commited");		
+	}	
 }
 		
 		
