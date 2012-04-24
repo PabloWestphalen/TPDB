d04945b99eaecfb60b99f680b3968cac9ec8d4ea
Pablo Westphalen <gotjin@gmail.com>
2012-04-24 12:17:47 -0300

    mvc?

--- src/main/java/com/jin/tpdb/controller/IndexController.java
+++ src/main/java/com/jin/tpdb/controller/IndexController.java
@@ -14,11 +14,26 @@ public class IndexController extends HttpServlet {
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("jin");
 		EntityManager em = factory.createEntityManager();		
 		
+		
 		String awesome = "secret powerful message =OO";
-		request.setAttribute("awesome", awesome);
+		
+		Macaco monkey = new Macaco();		
+		request.setAttribute("monkey", monkey);
 		request.setCharacterEncoding("UTF-8");
 		RequestDispatcher jsp = request.getRequestDispatcher("index.jsp");
 		jsp.forward(request, response);
 		
 	}	
+}
+
+private class Macaco {
+	private String nome = "Macacolouco";
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 }
