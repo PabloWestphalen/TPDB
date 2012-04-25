1b89e03ed0d3429a99abbbbefdfd0a4367c263a0
Pablo Westphalen <gotjin@gmail.com>
2012-04-25 16:08:25 -0300

    adding dao

--- src/main/java/com/jin/tpdb/controller/IndexController.java
+++ src/main/java/com/jin/tpdb/controller/IndexController.java
@@ -2,30 +2,29 @@ package com.jin.tpdb.controller;
 
 import com.jin.tpdb.entities.News;
 import com.jin.tpdb.entities.Album;
+import com.jin.tpdb.DAO;
 import java.io.*;
 import java.util.List;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.persistence.*;
-//import org.hibernate.Session;
-
 
 public class IndexController extends HttpServlet {
 	
+	private EntityManagerFactory factory = Persistence.createEntityManagerFactory("jin");
+	EntityManager em = factory.createEntityManager();	
+	
+	
+	public <T> List<T> getList(Class entity) {
+		return em.createQuery("SELECT n FROM " + entity.getName() + " n").getResultList();
+	}
+	
 	public void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("jin");
-		EntityManager em = factory.createEntityManager();		
-		
-		
-		/*
-		
-		Session hbs = (Session) em.getDelegate();List<News> newsList = hbs.createCriteria(News.class).list();
-		List<Album> albumsList = hbs.createCriteria(Album.class).list();*/
-		
-		List<News> newsList = em.createQuery("SELECT n FROM News n").getResultList();
-		List<Album> albumsList = em.createQuery("SELECT a FROM Album a").getResultList();
-		
+	
+		//List<News> newsList = em.createQuery("SELECT n FROM News n").getResultList();
+		List<News> newsList = getList(News.class);
+		List<Album> albumsList = em.createQuery("SELECT a FROM Album a").getResultList();		
 		
 		request.setAttribute("news", newsList);
 		request.setAttribute("albums", albumsList);
