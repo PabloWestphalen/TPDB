3e66c7509190b79170627ec6c4ef13c3eba50b08
Pablo Westphalen <gotjin@gmail.com>
2012-04-25 13:58:28 -0300

    adding dao

--- src/main/java/com/jin/tpdb/controller/IndexController.java
+++ src/main/java/com/jin/tpdb/controller/IndexController.java
@@ -2,46 +2,33 @@ package com.jin.tpdb.controller;
 
 import com.jin.tpdb.entities.News;
 import com.jin.tpdb.entities.Album;
-
 import java.io.*;
+import java.util.List;
 import javax.servlet.*;
 import javax.servlet.http.*;
-import java.util.List;
-
-import org.hibernate.Criteria;
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.AnnotationConfiguration;
-import org.hibernate.criterion.Order;
-
+import javax.persistence.*;
+//import org.hibernate.Session;
 
 
 public class IndexController extends HttpServlet {
 	
-	protected SessionFactory sessionFactory;
-	protected Session hbs = null;
-	protected Transaction transaction = null;
-	
 	public void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		
-		/*EntityManagerFactory factory = Persistence.createEntityManagerFactory("jin");
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("jin");
 		EntityManager em = factory.createEntityManager();		
-		Session hbs = (Session) em.getDelegate();*/
 		
-		sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
-		hbs = sessionFactory.openSession();
-		//transaction = session.beginTransaction();
 		
-		List<News> newsList = hbs.createCriteria(News.class).list();
-		List<Album> albumsList = hbs.createCriteria(Album.class).list();
+		/*
+		
+		Session hbs = (Session) em.getDelegate();List<News> newsList = hbs.createCriteria(News.class).list();
+		List<Album> albumsList = hbs.createCriteria(Album.class).list();*/
+		
+		List<News> newsList = em.createQuery("SELECT n FROM News n").getResultList();
+		List<Album> albumsList = em.createQuery("SELECT a FROM Album a").getResultList();
+		
 		
 		request.setAttribute("news", newsList);
 		request.setAttribute("albums", albumsList);
-		
-		//transaction.commit();
-		hbs.close();
 		
 		request.setCharacterEncoding("UTF-8");
 		RequestDispatcher jsp = request.getRequestDispatcher("index.jsp");
