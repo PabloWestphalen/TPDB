270585e23235433f9b310e872dbc1c420dace21f
Pablo Westphalen <gotjin@gmail.com>
2012-04-28 23:47:52 -0300

    adding real dao

--- src/main/java/com/jin/tpdb/persistence/DAO.java
+++ src/main/java/com/jin/tpdb/persistence/DAO.java
@@ -54,8 +54,9 @@ public class DAO {
 	public static <T> T load(Class c, int i) {
 		DAO dao = new DAO();
 		dao.open();
+		T result = dao.get(c, i);
 		dao.close();
-		return (T)dao.find(c, i);		
+		return result;		
 	}
 	
 	public static <T> List<T> getList(Class c) {		
@@ -63,8 +64,12 @@ public class DAO {
 		dao.open();
 		List<T> results = dao.getList(c);
 		dao.close();
-		return results;
+		return result;
 	}	
+	
+	public <T> T get(Class c, int i) {
+		return (T)em.find(c, i);
+	}
 	
 	protected <T> List<T> list(Class entity) {		
 		CriteriaBuilder cb = em.getCriteriaBuilder();
