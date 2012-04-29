a1cb88c61479c182c475c9f848c19a104b943533
Pablo Westphalen <gotjin@gmail.com>
2012-04-29 19:26:28 -0300

    fixing featured albums

--- src/main/java/com/jin/tpdb/persistence/DAO.java
+++ src/main/java/com/jin/tpdb/persistence/DAO.java
@@ -94,7 +94,8 @@ public class DAO {
 						FROM albums, artists, users
 						WHERE artists.artist_id = albums.artist_id AND users.user_id
 		*/
-		String qs = "SELECT COUNT(ac) FROM AlbumComment ac";
+		String qs = "SELECT COUNT(ac) FROM AlbumComment ac WHERE ac.Album.album_id = ?2";
+		// num  = 2
 		TypedQuery<Long> query = em.createQuery(qs, Long.class);
 		return query.getSingleResult();		
 		/*Root<AlbumComment> root = cq.from(AlbumComment.class);		
