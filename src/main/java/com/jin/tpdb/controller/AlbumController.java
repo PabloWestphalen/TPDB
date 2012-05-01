6954bdf2ee6bdd1b9707941e0f71656907ba1f9d
Pablo Westphalen <gotjin@gmail.com>
2012-05-01 19:49:05 -0300

    hibernating again

---
+++ src/main/java/com/jin/tpdb/controller/AlbumController.java
@@ -0,0 +1,45 @@
+package com.jin.tpdb.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.jin.tpdb.entities.Album;
+import com.jin.tpdb.persistence.DAO;
+
+/**
+ * Servlet implementation class AlbumController
+ */
+@WebServlet("/album/*/")
+public class AlbumController extends HttpServlet {
+
+	public void processRequest(HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
+		Album album = DAO.load(Album.class,
+				Integer.parseInt(request.getAttribute("id").toString()));
+
+		request.setAttribute("album", album);
+		request.setCharacterEncoding("UTF-8");
+		RequestDispatcher jsp = request.getRequestDispatcher("album.jsp");
+		jsp.forward(request, response);
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+}
