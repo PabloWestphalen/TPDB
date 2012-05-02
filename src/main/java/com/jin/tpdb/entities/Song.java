9aae8e67f09b2e5ea73b05112f50ce5fea9a589a
Pablo Westphalen <gotjin@gmail.com>
2012-05-01 21:36:12 -0300

    trying to get songs

--- src/main/java/com/jin/tpdb/entities/Song.java
+++ src/main/java/com/jin/tpdb/entities/Song.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Song {
@@ -12,6 +13,7 @@ public class Song {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	int id;
 
+	@ManyToOne
 	private Album album;
 
 	private int number;
