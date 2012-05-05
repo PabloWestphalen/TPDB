6647785841b49475642e692f1f6fa3bea6780289
Pablo Westphalen <gotjin@gmail.com>
2012-05-05 17:54:48 -0300

    debugging

---
+++ src/main/java/com/jin/tpdb/controller/ArtistsController.java
@@ -0,0 +1,39 @@
+package com.jin.tpdb.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.jin.tpdb.entities.Artist;
+import com.jin.tpdb.persistence.DAO;
+
+public class ArtistsController extends HttpServlet {
+	public void processRequest(HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
+
+		List<Artist> artists = DAO.getList(Artist.class);
+		request.setAttribute("artists", artists);
+
+		request.setCharacterEncoding("UTF-8");
+		RequestDispatcher jsp = request.getRequestDispatcher("artists.jsp");
+		jsp.forward(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+}
---
+++ src/main/webapp/artists.jsp
@@ -0,0 +1,12 @@
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+<%@taglib tagdir="/WEB-INF/tags" prefix="tpdb"%>
+<tpdb:page name="artists">
+  <tpdb:content>
+    <h2>Artists</h2>
+    <c:forEach var="artist" items="artists">
+      <p>${artist.name}</p>
+    </c:forEach>
+  </tpdb:content>
+  <tpdb:sidebar />
+</tpdb:page>
