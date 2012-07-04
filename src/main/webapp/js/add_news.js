121c8d838d42aee0e34ec413986626153e416fcd
Pablo Westphalen <gotjin@gmail.com>
2012-07-04 08:12:34 -0300

    addind a bit of an tags api

---
+++ src/main/webapp/js/add_news.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	   $('#tags').textext({
+	        plugins : 'tags prompt focus autocomplete ajax arrow',
+	        prompt : 'Add one...',
+	        ajax : {
+	            url : '/api',
+	            dataType : 'json',
+	            cacheResults : true
+	        }
+	    });
+});
