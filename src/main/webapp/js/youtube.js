6cd9f828de83909998b1acc2f3be610eff53f6d3
Pablo Westphalen <gotjin@gmail.com>
2012-08-18 16:33:30 -0300

    Commit from JBoss Tools

--- src/main/webapp/js/youtube.js
+++ src/main/webapp/js/youtube.js
@@ -9,11 +9,11 @@ var player;
 var playUrl;
 var loaded = false;
 $(document).ready(function() {
-/*	$('#player').addClass('playerLoading');
+	$('#player').addClass('playerLoading');
 	var tag = document.createElement('script');
 	tag.src = "https://www.youtube.com/iframe_api";
 	var firstScriptTag = document.getElementsByTagName('script')[0];
-	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);*/
+	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 	$('#tracksListing li a').click(function(event) {
 		event.preventDefault();
 	});
@@ -29,26 +29,14 @@ function onYouTubeIframeAPIReady() {
 	});
 }
 
-function playVideo() {
-	//player.loadVideoById(playUrl);
-}
-
 function onPlayerReady(evt) {
-	//evt.target.playVideo();
 	$('#tracksListing li').click(function(event) {
 		var video = $(this).data('video');
 		if (video != null) {
 			player.loadVideoById(video);
 			$('#player').removeClass('playerLoading');
 			$('#player').fadeIn();
-			//$('#player').fadeIn();
-			//onPlayerReady(null);
 		}
 	});
 	$('#player').fadeOut(0);
-}
-function onPlayerStateChange(evt) {
-}
-function stopVideo() {
-	player.stopVideo();
 }
