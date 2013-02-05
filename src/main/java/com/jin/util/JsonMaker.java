ac5f9441c9871e42f8f2de7dd53cb65f8ff88fc7
Pablo Westphalen <gotjin@gmail.com>
2013-02-05 17:28:33 -0200

    Commit from JBoss Tools

--- src/main/java/com/jin/util/JsonMaker.java
+++ src/main/java/com/jin/util/JsonMaker.java
@@ -2,174 +2,248 @@ package com.jin.util;
 
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.util.Collection;
-import java.util.Comparator;
+import java.util.IdentityHashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.TreeMap;
+import java.util.Set;
 
 public class JsonMaker {
-	int depth = 0;
+	private static Map<Object, Long> _objsVisited = new IdentityHashMap<Object, Long>();
+	private static long _identity = 0;
 
-	public static String from(Map<?, ?> m) {
-		if(m instanceof TreeMap == false) {
-			TreeMap<Object, Object> tm = new TreeMap<Object, Object>(new AlphabeticSort());
-			tm.putAll(m);
-			return toJson(tm, 0);
-		} else {
-			return toJson(m, 0);
-		}
-	}
-
-	public static String from(Collection<?> l) {
-		return toJson(l, 0);
-	}
 
 	public static String serialize(Object o) {
-		return serialize(o, 0);
-	}
-
-	private static String getter2field(String name) {
-		if (name.startsWith("get")) {
-			return String.valueOf(name.charAt(3)).toLowerCase()
-					+ name.substring(4, name.length());
-		}
-		if (name.startsWith("is")) {
-			return String.valueOf(name.charAt(2)).toLowerCase()
-					+ name.substring(3, name.length());
-
-		}
-		return null;
-	}
-
-	public static String serialize(Object o, int depth) {
-		System.out.println("Current level of recursion is " + depth++);
-		TreeMap<Object, Object> map = new TreeMap<Object, Object>(
-				new AlphabeticSort());
-		System.out.println("Dealing with a " + o.getClass().getName());
-		for (Method m : o.getClass().getMethods()) {
-			if (m.getDeclaringClass().equals(o.getClass())) {
-				if (m.getName().startsWith("get")
-						|| m.getName().startsWith("is")) {
-					for (Field f : o.getClass().getDeclaredFields()) {
-						String fieldName = getter2field(m.getName());
-						if (f.getName().equals(fieldName)) {
-							try {
-								f.setAccessible(true);
-								Object val = f.get(o);
-								System.out
-										.println("About to call toJson for the field \""
-												+ fieldName
-												+ "\" of class \""
-												+ o.getClass().getName() + "\"");
-								// System.out.println("Val is" + toJson(val));
-								map.put(fieldName, val);
-								System.out.println("Map atm is " + map);
-
-							} catch (IllegalArgumentException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							} catch (IllegalAccessException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-						}
-					}
-				}
-			}
-		}
-		if (map.size() > 0) {
-			System.out.println("Done dealing with \"" + o.getClass().getName()
-					+ "\"");
-			return toJson(map, depth);
-		} else {
-			return "\"" + o.toString() + "\"";
-		}
-	}
-
-	private static String toJson(Object o, int depth) {
 		StringBuilder json = new StringBuilder();
-		if (o != null) {
-			if (o instanceof Map) {
-				json.append("{");
-				Iterator<?> it = ((Map<?, ?>) o).entrySet().iterator();
-				while (it.hasNext()) {
-					Entry<?, ?> key = ((Entry<?, ?>) it.next());
-					json.append(toJson(key.getKey(), depth) + ": "
-							+ toJson(key.getValue(), depth));
-					if (it.hasNext()) {
-						json.append(",");
-					}
-				}
-				json.append("}");
-			} else if (o instanceof Number) {
-				json.append(o);
-			} else if (o instanceof String) {
-				json.append("\"" + o + "\"");
-			} else if (o instanceof Collection) {
-				json.append("[");
-				Iterator<?> it = ((Collection<?>) o).iterator();
-				while (it.hasNext()) {
-					json.append(toJson(it.next(), depth));
-					if (it.hasNext()) {
-						json.append(",");
-					}
-				}
-				json.append("]");
-			} else if (o instanceof Enum) {
-				json.append("\"" + o + "\"");
-			}
-
-			else if (o instanceof Object[]) {
-				if (o.getClass().isArray()) {
-					json.append("[");
-				}
-				// deal with array of objects
-				for (int i = 0; i < ((Object[]) o).length; i++) {
-
-					json.append("" + toJson(((Object[]) o)[i], depth) + "");
-					if (i + 1 < ((Object[]) o).length) {
-						json.append(",");
-					}
-				}
-				if (o.getClass().isArray()) {
-					json.append("]");
-				}
-			} else if (o.getClass().isArray()) {
-
-				// deal with array of primitives
-				json.append("[");
-				for (int i = 0; i < Array.getLength(o); i++) {
-					if (o instanceof char[]) {
-						json.append("\"" + Array.get(o, i) + "\"");
-					} else {
-						json.append(Array.get(o, i));
-					}
-					if (i + 1 < Array.getLength(o)) {
-						json.append(",");
-					}
-				}
-				json.append("]");
-			} else {
-				if (depth < 2) {
-					System.out.println("About to serialize "
-							+ o.getClass().getName());
-					json.append(serialize(o, depth));
-				} else {
-					json.append("\"" + o.toString() + "\"");
-				}
-			}
-			return json.toString();
+		json.append("{\n");
+		
+		//System.out.println("visiting object " + o.toString());
+		if(_objsVisited.containsKey(o)) {
+			////System.err.println("visitObject is returning REF becuase " + o.toString() + " has already been visited");
+			return "REF";
 		} else {
-			return "\"\"";
+			////System.err.println("visitObject put  " + o + " in the visited list");
+			_objsVisited.put(o, ++_identity);
+			json.append("\"@objectID\": " +  _objsVisited.get(o) + ",\n");
+			
 		}
+		Field[] fields = o.getClass().getDeclaredFields();
+		for(int i = 0; i < fields.length; i++) {
+			if(fields[i].getName().startsWith("this$")) {
+				continue;
+			}
+			try {
+				fields[i].setAccessible(true);
+				if(_objsVisited.containsKey(fields[i].get(o))) {
+					json.append("\"" + fields[i].getName() + "\" : \"@object" + _objsVisited.get(fields[i].get(o)) + "\"");
+					continue;
+				} else {
+					json.append("\"" + fields[i].getName() + "\": " + getValue(fields[i].get(o)));
+					
+				}
+				if(i+2 < fields.length) {
+					json.append(",\n");
+				}
+			} catch (Exception e) {
+				
+			}
+		}
+		json.append("}");
+		
+		//System.out.println("visitObject end");
+		return json.toString();
+		
 	}
-
-	static class AlphabeticSort implements Comparator<Object> {
-		public int compare(Object arg0, Object arg1) {
-			return arg0.toString().compareTo(arg1.toString());
+	
+	public static String getValue(Object o) {
+		//System.out.println("getting the value of " + o.getClass().getName() + " whose toString returns " + o.toString());
+		if(_objsVisited.containsKey(o)) {
+			//System.out.println("abort, because it has already been visited");
+			return "REF2";
+		} 
+		
+		if(o instanceof String || o instanceof Enum) {
+			//System.out.println("done. returning " + "\"" + o + "\"");
+			return "\"" + o + "\"";
+		} else if(o instanceof Number) {
+			return o.toString();
 		}
+		
+		else if(o instanceof Collection){
+			//System.out.println("is a collection, so i'll pass to the collection method");
+			return getCollectionValues((Collection<?>)o);
+		} else if(o instanceof Map) {
+			return getMap(o);
+		}
+		
+		else if(o instanceof Object[]) {
+			//array of objects
+			return getObjectArray(o);
+		} else if(o.getClass().isArray()) {
+			//array of primitives
+			return getArrayOfPrimitives(o);
+		}
+		//System.err.println("getV put " + o + " in the visited list");
+		// último else
+		String returnString = serialize(o);
+		//System.out.println("getV is returning " + returnString);
+		return returnString;
+	}
+	
+	public static String getObjectArray(Object o) {
+		StringBuilder json = new StringBuilder();
+		Object[] objs = (Object[])o;
+		
+		json.append("[");
+		for(int i = 0; i < objs.length; i++) {
+			json.append(getValue(objs[i]));
+			if(i+1 < objs.length) {
+				json.append(", ");
+			}
+		}
+		json.append("]");
+		
+		return json.toString();
+	}
+	
+	
+	public static String getMap(Object o) {
+		StringBuilder json = new StringBuilder();
+		json.append("{");
+		Iterator<?> it = ((Map<?, ?>) o).entrySet().iterator();
+		while (it.hasNext()) {
+			Entry<?, ?> key = ((Entry<?, ?>) it.next());
+			json.append(getValue(key.getKey()) + ": "
+					+ getValue(key.getValue()));
+			if (it.hasNext()) {
+				json.append(",");
+			}
+		}
+		json.append("}");
+		return json.toString();
+	}
+	
+	public static String getArrayOfPrimitives(Object o) {
+		StringBuilder json = new StringBuilder();
+		json.append("[");
+		for (int i = 0; i < Array.getLength(o); i++) {
+			if (o instanceof char[]) {
+				json.append("\"" + Array.get(o, i) + "\"");
+			} else {
+				json.append(Array.get(o, i));
+			}
+			if (i + 1 < Array.getLength(o)) {
+				json.append(", ");
+			}
+		}
+		json.append("]");
+		return json.toString();
+
+		
+	}
+	
+	public static String getCollectionValues(Collection<?> o) {
+		
+		
+		//System.out.println("parsing collection " + o.getClass().getName());
+		StringBuilder json = new StringBuilder();
+		Iterator<?> it = o.iterator();
+		json.append("[");
+		while(it.hasNext()) {
+			Object e = it.next();
+			if(_objsVisited.containsKey(e)) {
+				return "ABORTING PARSE OF COLLECTION VALUE " + e.toString();
+			}
+			//System.out.println("Passing '" + e + "' which is a " + e.getClass().getName() + " to getV");
+			json.append(getValue(e));
+			if(it.hasNext()) {
+				json.append(", ");
+			}
+		}
+		json.append("]");
+		_objsVisited.put(o, ++_identity);
+		return json.toString();
+	}
+	
+
+	public class Disco {
+		String titulo;
+		Artista artista;
+		
+
+		public Disco(String titulo, Artista artista) {
+			this.titulo = titulo;
+			this.artista = artista;
+		}
+
+		public String getTitulo() {
+			return titulo;
+		}
+
+		public void setTitulo(String titulo) {
+			this.titulo = titulo;
+		}
+
+		public Artista getArtista() {
+			return artista;
+		}
+
+		public void setArtista(Artista artista) {
+			this.artista = artista;
+		}
+		
+		public String toString() {
+			return titulo;
+		}
+
+	}
+	
+	enum Nacionalidade {
+		AMERICANO, BRASILEIRO
+	};
+
+	public class Artista {
+		String nome;
+		Set<Disco> discos;
+		int[] numeros = {1, 1, 2, 3, 5, 8};
+		char[] chars = { 'j', 'i', 'n' };
+		int idade = 22;
+		Nacionalidade nacionalidade = Nacionalidade.AMERICANO;
+		Map<Object, Object> mapa;
+		
+		public Map<Object, Object> getMapa() {
+			return mapa;
+		}
+
+		public void setMapa(Map<Object, Object> mapa) {
+			this.mapa = mapa;
+		}
+
+		public Artista(String nome) {
+			this.nome = nome;
+		}
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public Set<Disco> getDiscos() {
+			return discos;
+		}
+
+		public void setDiscos(Set<Disco> discos) {
+			this.discos = discos;
+		}
+		
+		@Override
+		public String toString() {
+			return nome;
+		}
+
 	}
 }
